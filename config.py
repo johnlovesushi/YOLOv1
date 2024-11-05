ecfb766f0272dcca26f2bfda0c4c2d2baedf0e1f
Ziqiao John Lin <linziqiao0620@gmail.com>
2024-11-04 21:38:38 -0800

    move some config vals into config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ CLASSES_PATH = os.path.join(DATA_PATH, 'classes.json')
 
 BATCH_SIZE = 64
 EPOCHS = 135
+VAL_FREQ = 5
 WARMUP_EPOCHS = 0
 LEARNING_RATE = 1E-4
 
@@ -14,7 +15,8 @@ EPSILON = 1E-6
 COCO_IMAGE_SIZE = (224,224)
 IMAGE_SIZE = (448, 448)
 NUM_WORKERS = 2
-
+IOU_THRESHOLD = 0.5
+CONF_THRESHOLD = 0.4
 S = 7       # Divide each image into a SxS grid
 B = 2       # Number of bounding boxes to predict
 C = 20      # Number of classes in the dataset
