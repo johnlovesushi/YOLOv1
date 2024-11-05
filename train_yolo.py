67f75abb61b27cc66f5486807cf38f3177bf0fe3
Ziqiao John Lin <linziqiao0620@gmail.com>
2024-11-04 21:39:50 -0800

    add mAP valuation into train and test, and wrap up train and test function

--- train_yolo.py
+++ train_yolo.py
@@ -3,8 +3,7 @@ from torch.utils.data import DataLoader
 from torch.autograd import Variable
 import shutil
 #from voc import VOCDataset
-from darknet import DarkNet
-from yolov1 import YOLOv1
+from models.yolov1net_resnet18 import YOLOv1_resnet18
 from loss import Loss
 from voc import VOCDataset
 import os
@@ -15,11 +14,12 @@ import config
 from tqdm import tqdm
 from tensorboardX import SummaryWriter
 from torch.utils.data import Subset
+import utils
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #assert device, 'Current implementation does not support CPU mode. Enable CUDA.'
-print('CUDA current_device: {}'.format(torch.cuda.current_device()))
-print('CUDA device_count: {}'.format(torch.cuda.device_count()))
+# print('CUDA current_device: {}'.format(torch.cuda.current_device()))
+# print('CUDA device_count: {}'.format(torch.cuda.device_count()))
 
 
 # Path to data dir.
@@ -33,39 +33,33 @@ val_label = 'data/voc2007test.txt'
 checkpoint_path = 'log/model_best.pth'
 
 # Frequency to print/log the results.
-print_freq = 5
 tb_log_freq = 5
 
 # Training hyper parameters.
 init_lr = 0.001
-base_lr = 0.01
 momentum = 0.9
 weight_decay = 5.0e-4
-num_epochs = 135
-batch_size = 64
-log_dir = './yolo_log'
 
+log_dir = './yolo_log'
+accum_iter = 16
 best_val_loss = float('inf')
 writer = SummaryWriter(log_dir=log_dir) 
-# Learning rate scheduling.
-def update_lr(optimizer, epoch, burnin_base, burnin_exp=4.0):
-    if epoch == 0:
-        lr = init_lr + (base_lr - init_lr) * math.pow(burnin_base, burnin_exp)
-    elif epoch == 1:
-        lr = base_lr
-    elif epoch == 75:
-        lr = 0.001
-    elif epoch == 105:
-        lr = 0.0001
-    else:
-        return
 
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+def update_lr(optimizer, epoch):
 
-def get_lr(optimizer):
-    for param_group in optimizer.param_groups:
-        return param_group['lr']
+    for g in optimizer.param_groups:
+        # 1. linear increase from 0.00001 to 0.0001 over 5 epochs
+        if epoch > 0 and epoch <= 5:
+            g['lr'] = 0.00001 +(0.00009/5) * (epoch)
+        # train at  0.0001 for 75 epochs 
+        if epoch <=80 and epoch > 5:
+            g['lr'] = 0.0001
+        # train at 0.00001 for 30 epochs
+        if epoch<= 110 and epoch> 80:
+            g['lr'] = 0.00001
+        # train until done
+        if epoch > 110:
+            g['lr'] = 0.000001
 
 def check_conv_layers(model):
     for i, (name, layer) in enumerate(model.named_children()):
@@ -92,150 +86,124 @@ def check_conv_layers(model):
         
     return
 
-def custom_collate_fn(batch):
-    images = torch.stack([item[0] for item in batch], dim=0)  # Stack images
-    targets = torch.stack([item[1] for item in batch], dim=0)  # Stack targets
-    return images, targets
-
-def train():
+def main():
     global best_val_loss
-    # Load pre-trained darknet.
-    darknet = DarkNet(conv_only=True, batch_norm=True, init_weight=True)
-    # darknet.features = torch.nn.DataParallel(darknet.features)  # not using right now
 
-    pretrained_dict = torch.load(checkpoint_path)['model_state_dict']
+    #print(device)
+    yolo = YOLOv1_resnet18().to(device)
 
-    # Load YOLO model.
-    yolo = YOLOv1(darknet.features).to(device)
-    yolo.conv_layers = torch.nn.DataParallel(yolo.conv_layers)
-
-    model_dict = yolo.state_dict()
-
-    print("=> before loading ")
-    check_conv_layers(yolo)
-    # Filter the pretrained_dict to include only layers related to 'Conv2d' and 'BatchNorm2d'
-    print('model dict weights')
-    # for key,val in model_dict:
-    #   print(key)
-    # print(model_dict.keys())
-    pretrained_dict_filtered = {k: v for k, v in pretrained_dict.items() if k in model_dict and 'features' in k}
-
-    # Update the model's weights
-    model_dict.update(pretrained_dict_filtered)
-    yolo.load_state_dict(model_dict)
-
-    print('darknet weight loading finished in yolo')
-    #model_dict = darknet.state_dict()
-
-    print("=> check yolo layers' weight ")
-    check_conv_layers(yolo)
-
-    # Setup loss and optimizer.
     criterion = Loss(feature_size=yolo.num_grids)
     optimizer = torch.optim.SGD(yolo.parameters(), lr=init_lr, momentum=momentum, weight_decay=weight_decay)
 
     # data loader
     train_dataset = VOCDataset(is_train = True, normalize = True)
-    train_loader = DataLoader(train_dataset, batch_size=config.BATCH_SIZE, shuffle=True, num_workers=config.NUM_WORKERS,drop_last=True,collate_fn=custom_collate_fn)
-
+    train_loader = DataLoader(train_dataset, batch_size=config.BATCH_SIZE//accum_iter, shuffle=True, num_workers=config.NUM_WORKERS,drop_last=True,collate_fn=utils.collate_function)
+    train_loader_mAP = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=config.NUM_WORKERS,drop_last=True,collate_fn=utils.collate_function)
+    
     val_dataset = VOCDataset(is_train = False, normalize = False)
-    val_loader = DataLoader(val_dataset, batch_size=config.BATCH_SIZE, shuffle=False, num_workers=config.NUM_WORKERS,drop_last=True,collate_fn=custom_collate_fn)
+    val_loader = DataLoader(val_dataset, batch_size=config.BATCH_SIZE//accum_iter, shuffle=False, num_workers=config.NUM_WORKERS,drop_last=True,collate_fn=utils.collate_function)
+    val_loader_mAP = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=config.NUM_WORKERS,drop_last=True,collate_fn=utils.collate_function)
 
-    # for data,target,label,_ in train_dataset:
-    #   print(data.dtype, target.dtype)
-      #print()
+    # initialization
+    train_loss_lst = []
+    val_loss_lst = []
+    train_mAP_lst = []
+    val_mAP_lst = []
 
-    # for data, targets in train_loader:
-    #       print(f"Input type: {images.dtype}, shape: {images.shape}")
-    #       print(f"Input type: {targets.dtype}, shape: {targets.shape}")  
-        #print([target.shape for target in data])  # Print each target's shape
-
-    # for images, targets,_,_ in train_loader:
-    #     print(images.shape)  # Print image batch shape
-    #     print([target.shape for target in targets])  # Print each target's shape
-    #exit()
     # Train
     for epoch in range(config.WARMUP_EPOCHS + config.EPOCHS):  # Define the number of epochs
-
-      yolo.train()  # Set the model to training mode for each epoch
-      epoch_loss = 0.0  # Initialize the loss for the epoch
-      total_train = 0
-      correct_train = 0
-      num_batches = len(train_loader)  # Get the number of batches
-
-
-      with tqdm(total=num_batches, desc=f"Epoch {epoch + 1}/{num_epochs}", unit="batch") as pbar:
-        for batch_idx, (images,targets) in enumerate(train_loader):
-            update_lr(optimizer, epoch, float(batch_idx) / float(len(train_loader) - 1))
-            lr = get_lr(optimizer)
-            # print(f"Input type: {images.dtype}, shape: {images.shape}")
-            # print(f"Input type: {targets.dtype}, shape: {targets.shape}")            
-            images, targets = images.to(device), targets.to(device)
-            # print(f"Input type: {images.dtype}, shape: {images.shape}")
-            # print(f"Input type: {targets.dtype}, shape: {targets.shape}")
-            predicted = yolo(images)  # Forward pass through the model
-
-            # print(f"Outputs shape: {outputs.shape}")  # Check the shape of outputs
-            # print(f"Targets shape: {targets.shape}")  # Check the shape of targets
-
-            loss = criterion(predicted, targets)  # Compute the loss
-            optimizer.zero_grad()  # Clear the gradients
-            loss.backward()  # Backpropagation
-            optimizer.step()  # Update weights
-
-            epoch_loss += loss.item()  # Accumulate the loss
-
-
-            total_train += targets.size(0)
-            correct_train += (predicted == targets).sum().item()
-            # Calculate and print the progress percentage
-                # Update tqdm progress bar and display loss
-            pbar.set_postfix({"Loss": f"{loss.item():.4f}"})
-            pbar.update(1)  # Update the progress bar by 1 batch
-
-        # Calculate train loss
-        avg_train_loss = epoch_loss / num_batches
-
-        # Log training metrics to TensorBoard
-        writer.add_scalar('Train/Loss', avg_train_loss, epoch)
-
-      # After each epoch, print summary
-      print(f"Epoch [{epoch + 1}/{num_epochs}] completed. Average Loss: {epoch_loss / num_batches:.4f}\n")
-
-      # Validation
-      if epoch % 4 == 0:  # every 4 epoch will evaluate one time
-        yolo.eval()       # Set model to evaluation mode
-        running_val_loss = 0.0
-        val_total = 0
         
-        with torch.no_grad():  # Disable gradient calculations for validation
-          for images, targets in val_loader:
-            images, targets = images.to(device).float(), targets.to(device).float()
-            
-            outputs = yolo(images)  # Forward pass
-            loss = criterion(outputs, targets)  # Compute validation loss
-            running_val_loss += loss.item()
+        # update learning rate
+        update_lr(optimizer, epoch)
 
-            # Calculate loss
-            _, predicted = torch.max(outputs.data, 1)  # Get class with highest score
-            val_total += targets.size(0)
-
-        avg_val_loss = running_val_loss / len(val_loader)
-
+        # Train
+        train_loss = train(train_loader, yolo, optimizer,  criterion, epoch)
+        train_mAP = utils.evalute_map(train_loader_mAP, yolo, config.CONF_THRESHOLD, config.IOU_THRESHOLD_THRESHOLD)
+        train_loss_lst.append(train_loss.item())
+        train_mAP_lst.append(train_mAP)
+        
+        # Validation
+        if (epoch+1)%config.VAL_FREQ == 0:
+            test_loss = test(val_loader, yolo, optimizer,  criterion)
+            test_mAP = utils.evalute_map(val_loader_mAP, yolo, config.CONF_THRESHOLD, config.IOU_THRESHOLD_THRESHOLD)
+        
+            print(f"Learning Rate:", optimizer.param_groups[0]["lr"])
+            print(f"Epoch:{epoch + 1 }  Train[Loss:{train_loss} mAP:{train_mAP}]  Test[Loss:{test_loss} mAP:{test_mAP}]")
+    
 
         # Log validation metrics to TensorBoard
-        writer.add_scalar('Val/Loss', avg_val_loss, epoch)
+        # writer.add_scalar('Val/Loss', avg_val_loss, epoch)
 
-        print(f"Validation Loss: {avg_val_loss:.4f}\n")
+        # print(f"Validation Loss: {avg_val_loss:.4f}\n")
 
-        is_best = avg_val_loss < best_val_loss
-        best_val_loss = min(avg_val_loss,best_val_loss)
-        state = {
-              'epoch': epoch + 1,
-              'model_state_dict': yolo.state_dict(),
-              'optimizer_state_dict': optimizer.state_dict()
-              }
-        save_checkpoint(state, is_best, log_dir, filename='checkpoint.pth')
+        # is_best = avg_val_loss < best_val_loss
+        # best_val_loss = min(avg_val_loss,best_val_loss)
+        # state = {
+        #       'epoch': epoch + 1,
+        #       'model_state_dict': yolo.state_dict(),
+        #       'optimizer_state_dict': optimizer.state_dict()
+        #       }
+        # save_checkpoint(state, is_best, log_dir, filename='checkpoint.pth')
+
+def train(train_loader, model, optimizer, loss_f, epoch):
+    """
+    This function is to run the training process and calculate the loss for single epoch
+    Input: 
+        train loader (torch loader)
+        model (torch model)
+        optimizer (torch optimizer)
+        loss function (torch custom yolov1 loss)
+        epoch (int) current epoch number
+    Output: loss (torch float).
+    """
+    model.train()  # Set the model to training mode for each epoch
+    epoch_loss = 0.0  # Initialize the loss for the epoch
+    total_train = 0
+    correct_train = 0
+    num_batches = len(train_loader)  # Get the number of batches
+    
+    with tqdm(total=num_batches, desc=f"Epoch {epoch + 1}/{config.EPOCHS}", unit="batch") as pbar:
+        for batch_idx, (data,targets, filename) in enumerate(train_loader):
+            tgs = torch.cat([target['yolo_targets'].unsqueeze(0).float().to(device)for target in targets], dim=0)
+            im = torch.cat([im.unsqueeze(0).float().to(device) for im in data], dim=0)
+            
+            with torch.set_grad_enabled(True):
+                preds = model(im)
+                loss = loss_f(preds, tgs)  # Compute the loss
+                # optimizer.zero_grad()  # Clear the gradients
+                epoch_loss += loss.item()
+                loss.backward()  # Backpropagation
+
+                if ((batch_idx + 1) % accum_iter == 0) or (batch_idx + 1 == len(train_loader)):
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+                pbar.set_postfix({"Loss": f"{loss.item():.4f}"})
+                pbar.update(1)  # Update the progress bar by 1 batch
+        
+        return epoch_loss/num_batches
+
+def test(test_loader, model, loss_f):
+    """
+    This function is to run the testing process and calculate the loss for single epoch
+    Input: test loader (torch loader), model (torch model), loss function 
+          (torch custom yolov1 loss).
+    Output: test loss (torch float).
+    """
+    epoch_loss = 0.0
+    model.eval()
+    num_batches = len(test_loader)
+    with torch.no_grad():
+        for batch_idx, (data,targets, filename) in enumerate(test_loader):
+            tgs = torch.cat([target['yolo_targets'].unsqueeze(0).float().to(device)for target in targets], dim=0)
+            im = torch.cat([im.unsqueeze(0).float().to(device) for im in data], dim=0)
+            
+            preds = model(im)
+            loss = loss_f(preds, tgs)  # Compute the loss
+            # optimizer.zero_grad()  # Clear the gradients
+            epoch_loss += loss.item()
+    
+    return epoch_loss/num_batches
 
 def save_checkpoint(state, is_best, log_dir, filename='checkpoint.pth'):
     path = os.path.join(log_dir, filename)
@@ -245,5 +213,5 @@ def save_checkpoint(state, is_best, log_dir, filename='checkpoint.pth'):
         shutil.copyfile(path, path_best)
     return
 if __name__ == '__main__':
-  train()
+    main()
 
